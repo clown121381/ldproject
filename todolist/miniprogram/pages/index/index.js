1bcd6beb71c48c5ed8e6bf1842499934d6d2997d
fengchuanyu <fengchuanyu@163.com>
2019-11-20 10:46:43 +0800

    admin

--- todolist/miniprogram/pages/index/index.js
+++ todolist/miniprogram/pages/index/index.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    todoList:[
+    initId:103,
+    inpVal:"",
+    todoList:[ //当前列表数据
       {
         title:"测试一",
         id:"101",
@@ -23,7 +25,7 @@ Page({
       }
     ]
   },
-
+  //选中当前行
   checkItem(e){
     let nowIndex = e.currentTarget.dataset.id;
     let thisList = this.data.todoList;
@@ -41,6 +43,7 @@ Page({
     })
   
   },
+  //移除当前行
   doneHandle(e){
     let nowIndex = e.currentTarget.dataset.id;
     let thisList = this.data.todoList;
@@ -50,9 +53,47 @@ Page({
       }
     })
     console.log(thisList)
-    // this.setData({
-    //   todoList: thisList
-    // })
+    this.setData({
+      todoList: thisList
+    })
+  },
+  //移除选中项
+  doneCheck(){
+    let thisList = this.data.todoList;
+    thisList = thisList.filter((item)=>{
+      if(!item.isCheck){
+        return item
+      }
+    })
+    this.setData({
+      todoList: thisList
+    })
+  },
+  //文本框输入事件
+  inpHandle(e){
+    this.setData({
+      inpVal: e.detail.value
+    })
+  },
+  //id处理函数
+  idHandle(){
+    let nowid = this.data.initId;
+    this.setData({
+      initId: ++nowid
+    })
+    return nowid
+  },
+  //添加待办事项
+  addHandle(){
+    let thisList = this.data.todoList;
+    thisList.push({
+      title: this.data.inpVal,
+      isCheck:false,
+      id: this.idHandle()
+    })
+    this.setData({
+      todoList:thisList
+    })
   },
   /**
    * 生命周期函数--监听页面加载
